16e56a05f5d7d19d25bdcc1a9de5fbc77696f130
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-03-05 18:12:37 +0700

    Catatan sederehana

--- app/src/main/java/com/enigmacamp/hiltautomobile/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/hiltautomobile/MainActivity.kt
@@ -3,6 +3,19 @@ package com.enigmacamp.hiltautomobile
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ Mengimplementasikan dependency injection, memberikan keuntungan sebagai berikut
+  - Code yang reuseable
+  - Mempermudah refactoring
+  - Mempermudah testing
+
+  Beberapa catatan:
+  - butuh pemahaman yang kuat terkait konsep OOP
+  - DI di Android, melakukan proses code generation, lebih tepatnya class genertion,
+  kadang untuk yang pertama kali belajar, cukup sulit dipahami
+  - Beberapa code kadang perlu ekstra pemahaman
+
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
